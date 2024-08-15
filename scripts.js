10f650f47cab6411670ae59efeddabd7fdd33368
Ehsan Hasin <ehsanhasin@gmail.com>
2024-08-15 13:34:26 +0430

    adding all files in one time

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView
